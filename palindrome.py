9e1ced9d6b96ac4d37e55c5c8fa8a5ad12b293a3
Juan Jiménez <nefioxdev@gmail.com>
2021-11-07 19:56:33 -0600

    Tipado estático

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+# from typing import Tuple, list, Dict
+
+def is_palindrome(string: str) -> bool:
+    string = string.replace(' ', '').lower()
+    return string == string[::-1]
+
+def run():
+    print(is_palindrome(1000))
+
+if __name__ == '__main__':
+    run()
