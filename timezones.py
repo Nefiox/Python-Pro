236ca00780250c6a648534ef94d3f20c5cfb7b64
Juan Jiménez <nefioxdev@gmail.com>
2021-11-21 19:48:47 -0600

    Timezones

---
+++ timezones.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import pytz
+
+
+bogota_timezone = pytz.timezone("America/Bogota")
+bogota_date = datetime.now(bogota_timezone)
+print('Bogotá: ', bogota_date.strftime('%d/%m/%Y, %H:%M:%S'))
+
+
+mexico_timezone = pytz.timezone("America/Mexico_City")
+mexico_date = datetime.now(mexico_timezone)
+print('México: ', mexico_date.strftime('%d/%m/%Y, %H:%M:%S'))
+
+
+argentina_timezone = pytz.timezone("America/Argentina/Buenos_Aires")
+argentina_date = datetime.now(argentina_timezone)
+print('Argentina: ', argentina_date.strftime('%d/%m/%Y, %H:%M:%S'))
